9827546e6abfb71bee1611ca12e57213ba79e79e
macosta2015 <macosta2015@my.fit.edu>
2022-05-04 17:37:15 -0400

    input saved into local storage

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+//Declaring the Global variables
 var nameInput = document.getElementById('uname');
+
+//This code is to get the local storage and have it saved when refreshing
+document.getElementById("demo").innerHTML = localStorage.getItem("Country Name");
 
 //Function
 document.querySelector('form.input-group').addEventListener('submit', function (e) {
@@ -6,6 +10,7 @@ document.querySelector('form.input-group').addEventListener('submit', function (
     //Setting the local storage for the variable that was read
     localStorage.setItem('name3', nameInput.value);
     changeAPI()
+    printingValuesHTML()
 });
 
 //Function
@@ -13,16 +18,23 @@ function send() {
     //Setting the local storage for the variable that was read
     localStorage.setItem('name3', nameInput.value);
     changeAPI()
+    printingValuesHTML()
 }
 
-// Running API code
-var APIKey = "24956411e34fc55da6781bc2185e43c8";
-function changeAPI(){ 
+//Function
+function printingValuesHTML (){
+// Set Item
+localStorage.setItem("Country Name", nameInput.value);
+// Retrieve
+document.getElementById("demo").innerHTML = localStorage.getItem("Country Name");
+}
 
+// Function-Running API code
+function changeAPI(){
+    var APIKey = "24956411e34fc55da6781bc2185e43c8";
     fetch(queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + uname.value + "&appid=" + APIKey)
     .then(response => response.json())
     .then (data => console.log(data))
-    
     .catch(err => alert("Wrong city name"))
 }
 
